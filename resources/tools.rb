16673aa77edeafd208da0d9335de9024806ca265
Montague, Brent <bmontague@cvent.com>
2017-05-17 17:55:46 -0400

    Update tools resource to work with new resource declarations
    Addresses #64 for the tools resource

--- resources/tools.rb
+++ resources/tools.rb
@@ -19,9 +19,35 @@
 # limitations under the License.
 #
 
-actions :install
+resource_name 'octopus_deploy_tools'
+
+property :path, kind_of: String, name_attribute: true
+property :source, kind_of: String, required: true
+property :checksum, kind_of: String
+
 default_action :install
 
-attribute :path, kind_of: String, default: 'C:\\octopus', name_attribute: true
-attribute :source, kind_of: String, required: true
-attribute :checksum, kind_of: String
+action :install do
+  path = new_resource.path
+  checksum = new_resource.checksum
+  tools_source = new_resource.source
+
+  tools_zip = ::File.join(Chef::Config[:file_cache_path], 'OctopusTools.zip')
+
+  remote_file tools_zip do
+    action :create
+    source tools_source
+    checksum checksum if checksum
+  end
+
+  directory path do
+    action :create
+    recursive true
+  end
+
+  windows_zipfile path do
+    action :unzip
+    source tools_zip
+    not_if { ::File.exist?(::File.join(path, 'Octo.exe')) }
+  end
+end
