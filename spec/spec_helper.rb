580d83b335cb4a115e61d8f65a6806f9b3bb2541
Brent Montague <brent@bmontague.com>
2017-05-18 15:42:35 -0400

    Add unit test coverage results to the project (#97)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 # frozen_string_literal: true
 $LOAD_PATH.unshift File.dirname('./libraries')
+
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
