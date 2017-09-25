fdc887a52a62428a4f8767e554de9a75e0db4fd1
Montague, Brent <bmontague@cvent.com>
2017-09-25 11:22:21 -0700

    Update appveyor testing to actually use a database

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@
 name 'octopus-deploy'
 maintainer 'Montague, Brent'
 maintainer_email 'BMontague@cvent.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Handles installing Octopus Deploy Server &| Tentacle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/cvent/octopus-deploy-cookbook'
